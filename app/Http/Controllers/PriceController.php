7f5211cddb9df30ff6de2d15705e0e4d1823855d
Sheean <24FTT1083@student.pb.edu.bn>
2025-10-03 13:04:56 +0800

    adding more listing to todayspricepage also its /toydaysprice not prices

--- app/Http/Controllers/PriceController.php
+++ app/Http/Controllers/PriceController.php
@@ -13,7 +13,7 @@ class PriceController extends Controller
             'products' => [
                 [
                     'name' => 'Chicken Breast',
-                    'description' => 'Fresh + Quality Guaranteed',
+                    'description' => 'Fresh • Quality Guaranteed',
                     'stores' => [
                         [
                             'name' => 'Supa Save',
@@ -27,7 +27,7 @@ class PriceController extends Controller
                         [
                             'name' => 'Hua Ho',
                             'rating' => 4.4,
-                            'distance' => 1.5,
+                            'distance' => 1.8,
                             'currentPrice' => 3.90,
                             'originalPrice' => 4.10,
                             'travelTime' => 5,
@@ -41,6 +41,55 @@ class PriceController extends Controller
                             'originalPrice' => 3.10,
                             'travelTime' => 10,
                             'hours' => '9AM–8PM'
+                        ]
+                    ]
+                ],
+                // ADD MORE PRODUCTS HERE
+                [
+                    'name' => 'Ribeye Steak',
+                    'description' => 'Fresh • Quality Guaranteed',
+                    'stores' => [
+                        [
+                            'name' => 'Supa Save',
+                            'rating' => 4.2,
+                            'distance' => 2.1,
+                            'currentPrice' => 9.00,
+                            'originalPrice' => 10.00,
+                            'travelTime' => 6,
+                            'hours' => '8AM–9PM'
+                        ],
+                        [
+                            'name' => 'Soon Lee',
+                            'rating' => 4.0,
+                            'distance' => 3.2,
+                            'currentPrice' => 7.20,
+                            'originalPrice' => 8.50,
+                            'travelTime' => 10,
+                            'hours' => '9AM–8PM'
+                        ]
+                    ]
+                ],
+                [
+                    'name' => 'Beansprouts',
+                    'description' => 'Fresh • Quality Guaranteed',
+                    'stores' => [
+                        [
+                            'name' => 'Hua Ho',
+                            'rating' => 4.4,
+                            'distance' => 1.8,
+                            'currentPrice' => 1.80,
+                            'originalPrice' => 1.90,
+                            'travelTime' => 6,
+                            'hours' => '7:30AM–10PM'
+                        ],
+                        [
+                            'name' => 'Supa Save',
+                            'rating' => 4.2,
+                            'distance' => 2.1,
+                            'currentPrice' => 2.10,
+                            'originalPrice' => 2.40,
+                            'travelTime' => 10,
+                            'hours' => '8AM–9PM'
                         ]
                     ]
                 ]
